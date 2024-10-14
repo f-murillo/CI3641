c5bb12d213aa61851ee788784a7b4e455853c8a2
f-murillo <murillo.franc@gmail.com>
2024-10-14 15:17:15 -0400

    Update reto.hs

--- Códigos Examen 1/Retro Extra/reto.hs
+++ Códigos Examen 1/Retro Extra/reto.hs
@@ -1,4 +1,4 @@
-import Math.Combinatorics.Exact.Binomial (choose)
-import Data.Bits (shiftR)
+import Math.Combinatorics.Exact.Binomial(choose)
+import Data.Bits(shiftR)
 trib n = if n<3 then n else tribs!!fromIntegral n where tribs=0:1:2:zipWith3(\a b c->a+b+c)tribs(tail tribs)(drop 2 tribs)
 maldad n = let pisoLog2 x = fromIntegral(length(takeWhile(>0)(iterate(`shiftR`1)x))-1) in trib(pisoLog2((choose(fromIntegral n)(fromIntegral(pisoLog2 n))*choose(fromIntegral n)(fromIntegral(pisoLog2 n -1)))`div`n)+1)
