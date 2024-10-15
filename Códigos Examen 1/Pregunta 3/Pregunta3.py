33c375e2b94314fc3f7e5dbbf498cf25cc27d68f
f-murillo <murillo.franc@gmail.com>
2024-10-14 20:41:53 -0400

    Update Pregunta3.py

--- Códigos Examen 1/Pregunta 3/Pregunta3.py
+++ Códigos Examen 1/Pregunta 3/Pregunta3.py
@@ -16,56 +16,63 @@ class Traductor:
         self.lenguaje_base = lenguaje_base
         self.lenguaje_origen = lenguaje_origen
         self.lenguaje_destino = lenguaje_destino
-
+        
 class Sistema:
     """Clase que implementa el sistema en el que se definen los programas, intérpretes y traductores"""
     def __init__(self):
-        # Diccionario para los programas, interpretes y traductores (más fácil para asociar valores con sus claves, y más eficiente para búsquedas)
+        # Diccionario para los programas, interpretes y traductores
         self.programas = {}
         self.interpretes = {}
         self.traductores = {}
 
     def definir_programa(self, nombre, lenguaje):
-        """Método que define un programa con un nombre 'a' y un lenguaje 'b' dados"""
-        # Si ya existe el programa
+        """Método que define un programa con un nombre 'a', escrito en un lenguaje 'b'"""
+        # Si el programa ya existe
         if nombre in self.programas:
             print(f"Error: ya existe un programa con el nombre '{nombre}'")
             return
+        # Crear programa
         self.programas[nombre] = Programa(nombre, lenguaje)
         print(f"Se definio el programa '{nombre}', ejecutable en '{lenguaje}'")
 
     def definir_interprete(self, lenguaje_base, lenguaje):
-        """Método que define un intérprete de un lenguaje 'a' escrito en un lennguaje 'b' dados"""
+        """Método que define un intérprete de un lenguaje, escrito en un lenguaje base"""
+        # Si el lenguaje de origen no esta en el diccionario de interpretes
         if lenguaje not in self.interpretes:
-            self.interpretes[lenguaje] = {} # Diccionario para tener varios interpretes para un mismo lenguaje
-        # Si ya el intérprete está definido
+            # Diccionario para poder tener varios interpretes para un mismo lenguaje de origen
+            self.interpretes[lenguaje] = {} 
+        # Si el lenguaje base ya esta definido para el lenguaje de origen
         if lenguaje_base in self.interpretes[lenguaje]:
             print(f"Error: ya existe un interprete para '{lenguaje}', escrito en '{lenguaje_base}'")
             return
-        
+        # Crear interprete
         self.interpretes[lenguaje][lenguaje_base] = Interprete(lenguaje_base, lenguaje)
         print(f"Se definio un interprete para '{lenguaje}', escrito en '{lenguaje_base}'")
 
     def definir_traductor(self, lenguaje_base, lenguaje_origen, lenguaje_destino):
+        """Método que define un traductor de un lenguaje de origen a un lenguaje destino, escrito en un lenguaje base"""
+        # Si el lenguaje de origen no esta en el diccionario de traductores
         if lenguaje_origen not in self.traductores:
-            self.traductores[lenguaje_origen] = {} # Diccionario para tener varios traductores para un mismo lenguaje
-
+            # Diccionario para poder tener varios traductores para un mismo lenguaje de origen
+            self.traductores[lenguaje_origen] = {} 
+        # Si el lenguaje base y destino ya estan definidos para el lenguaje de origen
         if lenguaje_destino in self.traductores[lenguaje_origen] and \
            self.traductores[lenguaje_origen][lenguaje_destino].lenguaje_base == lenguaje_base:
             print(f"Error: ya existe un traductor de '{lenguaje_origen}' hacia '{lenguaje_destino}', escrito en '{lenguaje_base}'")
             return
-
+        # Crear traductor
         self.traductores[lenguaje_origen][lenguaje_destino] = Traductor(lenguaje_base, lenguaje_origen, lenguaje_destino)
         print(f"Se definio un traductor de '{lenguaje_origen}' hacia '{lenguaje_destino}', escrito en '{lenguaje_base}'")
 
     def ejecutable(self, nombre):
         """Método que verifica si un programa es ejecutable"""
-        # Si el programa no está definido
+        # Si el programa no existe
         if nombre not in self.programas:
             print(f"Error: no existe un programa con el nombre '{nombre}'")
             return
-        lenguaje = self.programas[nombre].lenguaje # Lenguaje en el que fue escrito el programa
-        # Se llama al método recursivo ejecutableRec con el lenguaje
+        # Obtener el lenguaje en el que esta escrito el programa
+        lenguaje = self.programas[nombre].lenguaje
+        # Llamar a ejecutableRec sobre el lenguaje
         if self.ejecutableRec(lenguaje):
             print(f"Si, es posible ejecutar el programa '{nombre}'")
         else:
@@ -73,19 +80,21 @@ class Sistema:
 
     def ejecutableRec(self, lenguaje):
         """Método recursivo que determina si un programa es ejecutable, dado el lenguaje en el que fue escrito"""
-        if lenguaje == "LOCAL": # Caso base
+        if lenguaje == "LOCAL":  # Caso base
             return True
-        # Si el lenguaje está entre los intérpretes y si el método recursivo sobre la base del lenguaje retorna true
+        # Si el lenguaje está en el diccionario de intérpretes, y si para alguna base de algún intérprete del lenguaje el método recursivo retorna True
         if lenguaje in self.interpretes and any(self.ejecutableRec(base) for base in self.interpretes[lenguaje]):
             return True
-        # Si el lenguaje está entre los traductores y si el método recursivo sobre la base del lenguaje retorna true
-        if lenguaje in self.traductores and any(self.ejecutableRec(destino) for destino in self.traductores[lenguaje]):
+        # Si el lenguaje está en el diccionario de traductores, y si para alguna base y destino de algún traductor del lenguaje el método recursivo retorna True
+        if lenguaje in self.traductores and any(self.ejecutableRec(traductor.lenguaje_base) and \
+                self.ejecutableRec(destino) for destino, traductor in self.traductores[lenguaje].items()):
             return True
         # Si no se cumple ninguna condición, el programa no es ejecutable
-        return False
+        return False 
 
 def main():
     """Método Principal"""
+    # Crear el sistema
     sistema = Sistema()
     while True:
         accion = input("Introduce una accion: ")
@@ -95,6 +104,10 @@ def main():
         partes = accion.split()
         # Se verifica la accion ingresada
         if partes[0] == "DEFINIR":
+            # Si la accion es DEFINIR, y el numero total de parametros es menor a 4 o mayor a 5, se ingreso mal la accion
+            if len(partes) < 4 or len(partes) > 5:
+                print("Error: numero de parametros incorrecto")
+                continue
             tipo = partes[1]
             # Switch para verificar el tipo de la accion 
             match tipo:
