1c96270e9924bc6a34bf45ed33002e02a1d8ecd4
f-murillo <murillo.franc@gmail.com>
2024-10-31 21:08:03 -0400

    Add files via upload

---
+++ Códigos Examen 2/Pregunta 1 inciso b/Count.m
@@ -0,0 +1,29 @@
+% Método "principal"
+function Count()
+    n = input('Ingresa un número: ');
+    if n <= 0 || mod(n,1) ~= 0
+       disp('n tiene que ser un entero positivo'); 
+       return;
+    end
+    m = ['El count de ', num2str(n), ' es: ', num2str(c(n))];
+    disp(m);
+end
+
+% Método que calcula f(n) como se define en el enunciado de la pregunta
+function x = f(n)
+    if mod(n,2) == 0
+        x = n/2;
+    else
+        x = 3*n + 1;
+    end
+end
+
+% Método que cuenta el número de aplicaciones de f sobre n hasta que el
+% resultado sea 1
+function a = c(n)
+    a = 0;
+    while n ~= 1
+        n = f(n);
+        a = a + 1;
+    end
+end
