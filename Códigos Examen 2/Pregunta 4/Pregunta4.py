9400ff6ede43fe9b7f4316c7e107f0ed950f25cc
f-murillo <murillo.franc@gmail.com>
2024-11-04 09:02:43 -0400

    Update Pregunta4.py

--- Códigos Examen 2/Pregunta 4/Pregunta4.py
+++ Códigos Examen 2/Pregunta 4/Pregunta4.py
@@ -19,7 +19,7 @@ def f_3_3_iterativo(n):
     actual = 0
     # Iterar desde 9 hasta n + 1
     for _ in range(9, n + 1):
-        actual = a6 + a3 + a0 # Esta parte corresponde al argumento (a6 + a3 + a0) de la llamada recursiva a f_3_3_aux en la versión recursiva de cola
+        actual = a6 + a3 + a0 # Esta parte corresponde al argumento (a6 + a3 + a0) de la llamada a f_3_3_aux en la versión recursiva de cola
         # Actualizar los acumuladores
         a0, a1, a2, a3, a4, a5, a6, a7, a8 = a1, a2, a3, a4, a5, a6, a7, a8, actual
     return actual
