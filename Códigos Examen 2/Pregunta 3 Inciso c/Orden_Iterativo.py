11d428ddecdf8c77648dd8a3fb90d12e762cc905
f-murillo <murillo.franc@gmail.com>
2024-11-04 23:30:57 -0400

    Update Orden_Iterativo.py

--- Códigos Examen 2/Pregunta 3 Inciso c/Orden_Iterativo.py
+++ Códigos Examen 2/Pregunta 3 Inciso c/Orden_Iterativo.py
@@ -1,7 +1,5 @@
 def ins_ord(e, ls):
-    """
-    Método que inserta un elemento en su posición correcta dentro de una lista ordenada.
-    """
+    """Método que inserta un elemento en su posición correcta dentro de una lista ordenada"""
     if not ls or e <= ls[0]:
         yield [e] + ls
     else:
@@ -9,9 +7,7 @@ def ins_ord(e, ls):
             yield [ls[0]] + rest
 
 def ord(ls):
-    """
-    Método que ordena una lista de elementos utilizando inserciones recursivas
-    """
+    """Método que ordena una lista de elementos utilizando inserciones recursivas"""
     if ls:
         for sort_t in ord(ls[1:]):
             for sort_l in ins_ord(ls[0], sort_t):
@@ -20,9 +16,7 @@ def ord(ls):
         yield []
 
 def it_ord(ls):
-    """
-    Método que itera y devuelve los elementos de una lista en orden ascendente.
-    """
+    """Método que itera y devuelve los elementos de una lista en orden ascendente."""
     for sort_l in ord(ls):
         for item in sort_l:
             yield item
